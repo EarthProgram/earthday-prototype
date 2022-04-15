ed06d6dea66ed2c6f928ed0eeb1461388e455701
bhanugummana <62737055+bhanugummana@users.noreply.github.com>
2022-04-15 09:27:13 +0530

    Added css for radio buttons

--- components/selectLanguage.tsx
+++ components/selectLanguage.tsx
@@ -10,14 +10,17 @@ export default function SelectLanguage(onSelect) {
     <form className="radio-btn">
       {languages.map((item, index) => (
         <div key={index} className="block">
-          <input
-            name="radio-item-1"
-            value={item.code}
-            type="radio"
-            onChange={(e) => onSelect(e.target.value)}
-            defaultChecked={index === 0}
-          />
-          <label className="txt" htmlFor="radio-item-1">{item.name}</label>
+          <label className="txt radio-label">
+            <input
+            className="radio-input"
+              value={item.code}
+              onChange={(e) => onSelect(e.target.value)}
+              defaultChecked={index === 0}
+              type="radio"
+              name="radio"
+            />
+            {item.name}
+          </label>
         </div>
       ))}
     </form>
