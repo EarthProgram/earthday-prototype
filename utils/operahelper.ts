3795f6251022a6a6c4c81df3d967d7db46678441
Alwyn van Wyk <alwyn.vanwyk@ixo.world>
2022-05-12 10:15:40 +0200

    added Benzhe's transform

--- utils/operahelper.ts
+++ utils/operahelper.ts
@@ -74,6 +74,39 @@ function getAddress() {
     return address
 }
 
+
+function transformSignature(signatureOperaBase64) {
+    const rawArray = Base64.toUint8Array(signatureOperaBase64)
+    console.log("rawSignature", rawArray)
+
+    if (rawArray.length < 64 || rawArray.length > 66) {
+        console.log("invalid length")
+        return
+    }
+
+    let signatureCosmjsBase64 = ""
+
+    if (rawArray.length == 64) {
+        signatureCosmjsBase64 = signatureOperaBase64
+    } else if (rawArray.length == 65) {
+        if (rawArray[0] == 0x00) {
+            signatureCosmjsBase64 = Base64.fromUint8Array(rawArray.slice(1, 65))
+        } else if (rawArray[32] == 0x00) {
+            signatureCosmjsBase64 = Base64.fromUint8Array([rawArray.slice(0, 32), rawArray.slice(33, 65)])
+        } else {
+            console.log("invalid signature array, length 65")
+        }
+    } else if (rawArray.length == 66) {
+        if (rawArray[0] == 0x00 && rawArray[33] == 0x00) {
+            signatureCosmjsBase64 = Base64.fromUint8Array([rawArray.slice(1, 33), rawArray.slice(34, 66)])
+        } else {
+            console.log("invalid signature array, length 66")
+        }
+    }
+    console.log("signatureCosmjsBase64", signatureCosmjsBase64)
+    return signatureCosmjsBase64
+}
+
 export async function signOpera(toAddress: string) {
     const stdSignDoc = await ixohelper.getStdSignDoc(toAddress, getAddress())
     console.log("operahelper.stdSignDoc", stdSignDoc)
@@ -81,7 +114,9 @@ export async function signOpera(toAddress: string) {
     console.log("operahelper.sha256msg", sha256msg)
     const hexValue = Buffer.from(sha256msg).toString("hex")
     console.log("operahelper.hexValue", hexValue)
-    const signatureValue = await window.interchain.signMessage(hexValue, signMethodSECP256k1Opera, addressIndex)
+    const signatureOpera = await window.interchain.signMessage(hexValue, signMethodSECP256k1Opera, addressIndex)
+    console.log("operahelper.signatureOpera", signatureOpera)
+    const signatureValue = transformSignature(signatureOpera)
     console.log("operahelper.signatureValue", signatureValue)
     return { signed: stdSignDoc, signatureValue }
 }
