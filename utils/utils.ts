ecf4b3974349a0e1fc0f8386be60ec920625e1ca
Alwyn van Wyk <alwyn.vanwyk@ixo.world>
2022-06-21 11:25:44 +0200

    testing MsgBuy with SECP256k1

--- utils/utils.ts
+++ utils/utils.ts
@@ -86,57 +86,57 @@ export async function broadcastTransaction(toAddress: string) {
         console.log("utils.error - ixo-client-sdk - MsgBuy ED25519 --------", error)
     }
 /**************************** libraries (@cosmjs/amino + sovrin) ****************************/
-    // try {
-    //     console.log(" -------- cosmjs/amino - MsgSend SECP256k1 --------")
-    //     await aminohelper.init(ixohelper.mnemonic_CYC, ixohelper.prefix)
-    //     const { signed, signature } = await aminohelper.sign(
-    //         await messagehelper.getMsgSendStdSignDoc(aminohelper.address, toAddress))
-    //     const postResult = await ixohelper.postTransactionSECP(signed, signature, aminohelper.pubkeyBase64)
-    //     console.log("cosmjs/amino.postResult", postResult)
-    // } catch (error) {
-    //     console.log("utils.error - cosmjs/amino - MsgSend SECP256k1 ", error)
-    // }
+    try {
+        console.log(" -------- cosmjs/amino - MsgSend SECP256k1 --------")
+        await aminohelper.init(ixohelper.mnemonic_CYC, ixohelper.prefix)
+        const { signed, signature } = await aminohelper.sign(
+            await messagehelper.getMsgSendStdSignDoc(aminohelper.address, toAddress))
+        const postResult = await ixohelper.postTransactionSECP(signed, signature, aminohelper.pubkeyBase64)
+        console.log("cosmjs/amino.postResult", postResult)
+    } catch (error) {
+        console.log("utils.error - cosmjs/amino - MsgSend SECP256k1 ", error)
+    }
 
-    // try {
-    //     console.log(" -------- cosmjs/amino - MsgBuy SECP256k1 --------")
-    //     await aminohelper.init(ixohelper.mnemonic_CYC, ixohelper.prefix)
-    //     const { signed, signature } = await aminohelper.sign(
-    //         await messagehelper.getMsgBuyStdSignDoc(aminohelper.address, ixohelper.bondDID, ixohelper.buyerDID_CYC))
-    //     const postResult = await ixohelper.postTransactionSECP(signed, signature, aminohelper.pubkeyBase64)
-    //     console.log("cosmjs/amino.postResult", postResult)
-    // } catch (error) {
-    //     console.log("utils.error - cosmjs/amino - MsgSend SECP256k1 ", error)
-    // }
+    try {
+        console.log(" -------- cosmjs/amino - MsgBuy SECP256k1 --------")
+        await aminohelper.init(ixohelper.mnemonic_CYC, ixohelper.prefix)
+        const { signed, signature } = await aminohelper.sign(
+            await messagehelper.getMsgBuyStdSignDoc(aminohelper.address, ixohelper.bondDID, ixohelper.buyerDID_CYC))
+        const postResult = await ixohelper.postTransactionSECP(signed, signature, aminohelper.pubkeyBase64)
+        console.log("cosmjs/amino.postResult", postResult)
+    } catch (error) {
+        console.log("utils.error - cosmjs/amino - MsgSend SECP256k1 ", error)
+    }
 
-    // try {
-    //     console.log(" -------- sovrin - MsgBuy ED25519 --------")
-    //     await sovrinhelper.init(ixohelper.mnemonic_CYC, ixohelper.prefix)
-    //     const { signed, signature } = await sovrinhelper.sign(
-    //         await messagehelper.getMsgBuyStdSignDoc(sovrinhelper.address, ixohelper.bondDID, ixohelper.buyerDID_CYC))
-    //     const postResult = await ixohelper.postTransactionED(signed, signature, sovrinhelper.pubkeyBase64)
-    //     console.log("sovrin.postResult", postResult)
-    // } catch (error) {
-    //     console.log("utils.error - sovrin - MsgBuy ED25519", error)
-    // }
+    try {
+        console.log(" -------- sovrin - MsgBuy ED25519 --------")
+        await sovrinhelper.init(ixohelper.mnemonic_CYC, ixohelper.prefix)
+        const { signed, signature } = await sovrinhelper.sign(
+            await messagehelper.getMsgBuyStdSignDoc(sovrinhelper.address, ixohelper.bondDID, ixohelper.buyerDID_CYC))
+        const postResult = await ixohelper.postTransactionED(signed, signature, sovrinhelper.pubkeyBase64)
+        console.log("sovrin.postResult", postResult)
+    } catch (error) {
+        console.log("utils.error - sovrin - MsgBuy ED25519", error)
+    }
 /**************************** OPERA ****************************/
     try {
-        console.log(" -------- Opera - MsgSend SECP256k1 --------")
-        await operasecp256k1helper.init(ixohelper.prefix)
-        const { signed, signature } = await operasecp256k1helper.sign(
-            await messagehelper.getMsgSendStdSignDoc(operasecp256k1helper.address, toAddress))
-        const postResult = await ixohelper.postTransactionSECP(signed, signature, operasecp256k1helper.pubkeyBase64)
-        console.log("opera.postResult", postResult)
+        // console.log(" -------- Opera - MsgSend SECP256k1 --------")
+        // await operasecp256k1helper.init(ixohelper.prefix)
+        // const { signed, signature } = await operasecp256k1helper.sign(
+        //     await messagehelper.getMsgSendStdSignDoc(operasecp256k1helper.address, toAddress))
+        // const postResult = await ixohelper.postTransactionSECP(signed, signature, operasecp256k1helper.pubkeyBase64)
+        // console.log("opera.postResult", postResult)
     } catch (error) {
         console.log("utils.error - Opera - MsgSend SECP256k1 ", error)
     }
 
     try {
-        console.log(" -------- Opera - MsgBuy ED25519 --------")
-        await operaed25519helper.init(ixohelper.prefix)
-        const { signed, signature } = await operaed25519helper.sign(
-            await messagehelper.getMsgBuyStdSignDoc(operaed25519helper.address, ixohelper.bondDID, ixohelper.buyerDID_ECO))
-        const postResult = await ixohelper.postTransactionED(signed, signature, operaed25519helper.pubkeyBase64)
-        console.log("opera.postResult", postResult)
+        // console.log(" -------- Opera - MsgBuy ED25519 --------")
+        // await operaed25519helper.init(ixohelper.prefix)
+        // const { signed, signature } = await operaed25519helper.sign(
+        //     await messagehelper.getMsgBuyStdSignDoc(operaed25519helper.address, ixohelper.bondDID, ixohelper.buyerDID_ECO))
+        // const postResult = await ixohelper.postTransactionED(signed, signature, operaed25519helper.pubkeyBase64)
+        // console.log("opera.postResult", postResult)
     } catch (error) {
         console.log("utils.error - Opera - MsgBuy ED25519", error)
     }
